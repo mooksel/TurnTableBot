82ababa57ba4c07e6c50a0760b3e55fb9781d9c1
dmitry.antipenko <dmitrij.antipenko@gmail.com>
2018-07-04 16:44:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import telebot
 token = "513777236:AAGznaDind0_iIelgAnguG6cgJ3_22E71ZY"
 bot = telebot.TeleBot(token)
 
+'''
+shrug - Shrugs.
+turn_table - Turns table.
+take_my_energy - Gives you energy.
+lenny - Lenny.
+chlen - Visits your mama.
+disapproval - Make me unsee it!
+'''
 
 @bot.message_handler(commands=['shrug'])
 def handle_shrug(message):
@@ -17,14 +25,32 @@ def handle_turn_table(message):
 def handle_turn_table(message):
 	bot.send_message(message.chat.id, "༼ つ ◕_◕ ༽つ")
 
+@bot.message_handler(commands=['lenny'])
+def handle_turn_table(message):
+	bot.send_message(message.chat.id, "( ͡° ͜ʖ ͡°)")
+
+@bot.message_handler(commands=['chlen'])
+def handle_turn_table(message):
+	bot.send_message(message.chat.id, "8========3")
+
+@bot.message_handler(commands=['disapproval'])
+def handle_turn_table(message):
+	bot.send_message(message.chat.id, "ಠ_ಠ")
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+	if message.text.endswith("влад") or message.text.endswith("Влад"):
+		bot.send_message(message.chat.id, "ебал томат")
+	if message.text.endswith("дима") or message.text.endswith("Дима") or message.text.endswith("Димасик") or message.text.endswith("димасик"):
+		bot.send_message(message.chat.id, "пьет пивасик")
+	if message.text.endswith("алексей") or message.text.endswith("Алексей"):
+		bot.send_message(message.chat.id, "ебал гусей")
 	if message.text.endswith("мразота") or message.text == "иди нахуй":
 		bot.send_message(message.chat.id, "Проверяй за щекой")
 	if message.text.endswith("пидор"):
 		bot.send_message(message.chat.id, "Сам пидор")
-	elif "бля" in message.text or "сука" in message.text:
+	if "бля" in message.text or "сука" in message.text:
 		bot.send_message(message.chat.id, "И этими губами ты целуешь свою маму?")
 
 
